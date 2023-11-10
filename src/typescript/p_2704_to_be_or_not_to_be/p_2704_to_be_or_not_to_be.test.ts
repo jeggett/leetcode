e80a45ddd99ffe070f570cf6986f37f79e1709fa
Alexander Zaytsev <jeggettx@gmail.com>
2023-11-10 18:55:22 +0300

    feat: expect function

---
+++ src/typescript/p_2704_to_be_or_not_to_be/p_2704_to_be_or_not_to_be.test.ts
@@ -0,0 +1,8 @@
+import { expectFunc } from "./p_2704_to_be_or_not_to_be";
+
+test("problem 2704", () => {
+  expect(expectFunc(1).toBe(1)).toEqual(true);
+  expect(() => expectFunc(1).toBe(2)).toThrow(new Error("Not Equal"));
+  expect(expectFunc(1).notToBe(2)).toEqual(true);
+  expect(() => expectFunc(1).notToBe(1)).toThrow(new Error("Equal"));
+});
