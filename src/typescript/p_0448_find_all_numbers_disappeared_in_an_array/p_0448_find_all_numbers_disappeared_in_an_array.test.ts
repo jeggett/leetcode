4a50b7528c89d45a60142a4a0b656fd1188aa596
Alexander Zaytsev <jeggettx@gmail.com>
2023-11-23 22:15:57 +0300

    feat: find all number disappeared in an array

---
+++ src/typescript/p_0448_find_all_numbers_disappeared_in_an_array/p_0448_find_all_numbers_disappeared_in_an_array.test.ts
@@ -0,0 +1,6 @@
+import { findDisappearedNumbers } from "./p_0448_find_all_numbers_disappeared_in_an_array";
+
+test("problem 0448", () => {
+  expect(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1])).toEqual([5, 6]);
+  expect(findDisappearedNumbers([1, 1])).toEqual([2]);
+});
