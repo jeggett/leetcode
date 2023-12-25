40225e91f7e0bb68d0c89bcd5b6598afda12e024
Alexander Zaytsev <jeggettx@gmail.com>
2023-12-26 00:52:50 +0300

    feat(0283): move zeroes
    update some configs

---
+++ src/typescript/p_0283_move_zeroes/p_0283_move_zeroes.test.ts
@@ -0,0 +1,15 @@
+import { moveZeroes } from "./p_0283_move_zeroes";
+
+test("problem 0283", () => {
+  let nums = [0, 1, 0, 3, 12];
+  moveZeroes(nums);
+  expect(nums).toEqual([1, 3, 12, 0, 0]);
+
+  nums = [0];
+  moveZeroes(nums);
+  expect(nums).toEqual([0]);
+
+  nums = [1, 2, 3];
+  moveZeroes(nums);
+  expect(nums).toEqual([1, 2, 3]);
+});
